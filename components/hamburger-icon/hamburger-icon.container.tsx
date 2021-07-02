f0807ce60e57d66982d3070b1dd15c3eff5b5b84
Van den Berghe Jo <jo.van.den.berghe@gmail.com>
2021-07-03 01:47:23 +0200

    Update favicon to smartphtoo logo

--- components/hamburger-icon/hamburger-icon.container.tsx
+++ components/hamburger-icon/hamburger-icon.container.tsx
@@ -1,8 +1,6 @@
 import React, { memo, useState } from "react";
 import { HamburgerIcon } from "./hamburger-icon";
 
-import styles from "./hamburger-icon.module.css";
-
 export const HamburgerIconContainer = memo(function HamburgerIconContainer() {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
